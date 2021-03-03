cc061ae4f2c77363d045c25e5417fbe32dbb2f6b
Anyijukire <anyijukirjanett@gmail.com>
2021-03-03 08:24:40 +0300

    first commit

---
+++ src/ester.kt
@@ -0,0 +1,49 @@
+fun main() {
+    add()
+    subtract()
+    name()
+    multiplication()
+    modulus()
+    division()
+    compare()
+}
+fun add(){
+    var a=45
+    var b=34
+    var c=90
+    var sum=(a+b+c)
+    println(sum)
+}
+fun subtract(){
+    var d=45
+    var e=34
+    var subtract=(d-e)
+    println(subtract)
+}
+fun name(){
+    val name=("anyijukire janet")
+    println(name)
+}
+fun multiplication(){
+    var a=33
+    var b=20
+    var product=(a*b)
+    println(product)
+}
+fun modulus(){
+    var a=29
+    var b=7
+    var modulus=a%b
+    println(modulus)
+}
+fun division(){
+    var a=25
+    var b=5
+    var divide=a/b
+    println(divide)
+}
+fun compare(){
+    var a=60
+    var b=59
+    println(a<b)
+}
